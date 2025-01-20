19b4b387c17e2729a1c80603b3a55d1c91687a82
Purnendu76 <Purnendu76@gmail.com>
2025-01-20 22:56:32 +0530

    adding Map

--- src/app/component/partials/map/map.component.ts
+++ src/app/component/partials/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { icon, latLng, LatLngTuple, map, Map, tileLayer } from 'leaflet';
+ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { icon, LatLng, latLng, LatLngExpression, LatLngTuple, map, Map, marker, Marker, tileLayer } from 'leaflet';
 import { LocationService } from 'src/app/services/location.service';
 
 @Component({
@@ -9,46 +9,56 @@ import { LocationService } from 'src/app/services/location.service';
 
 })
 export class MapComponent  implements OnInit {
-private readonly MARKER_ZOOM_LEVEL=16;
-
-private readonly MARKER_ICON=icon({
+ private readonly MARKER_ZOOM_LEVEL =16;
+ private readonly MARKER_ICON=icon({
   iconUrl:
-      'https://res.cloudinary.com/foodmine/image/upload/v1638842791/map/marker_kbua9q.png',
-    iconSize: [42, 42],
-    iconAnchor: [21, 42],
-})
+  'https://res.cloudinary.com/foodmine/image/upload/v1638842791/map/marker_kbua9q.png',
+  iconSize: [42, 42],
+  iconAnchor: [21, 42],
+ })
+  private readonly DEFAULT_LATLNG:LatLngTuple=[13.75, 21.62]
+@ViewChild('map',{static:true})
+mapref!:ElementRef;
 
+map!:Map;
+currentMarker!:Marker;
+constructor(private locatopnervice:LocationService){
 
-  private readonly DEFAULT_LATLNG:LatLngTuple  =[13.75,21.62]
+}
   
-  @ViewChild('map',{static:true})
-  mapRef!:ElementRef;
-
-  map!:Map;
-  // currentMarker!Marker
-  constructor(private locationService:LocationService ){}
-
   ngOnInit(): void {
+
     this.initilizeMap()
   }
+
   initilizeMap(){
     if(this.map) return;
 
-    this.map= map(this.mapRef.nativeElement,{
+    this.map=map(this.mapref.nativeElement,{
       attributionControl:false
-
     }).setView(this.DEFAULT_LATLNG,1)
 
-    tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.map);
+    tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.map)
   }
-   
   findMyLocation(){
-     this.locationService.getCurrentLocation().subscribe({
-      next:(latLng) =>{
-        console.log(latLng);
-        
-      }
-     })
+   this.locatopnervice.getCurrentLocation().subscribe({
+    next:(latLng)=>{
+      this.map.setView(latLng,this.MARKER_ZOOM_LEVEL)
+      // console.log(latLng);
+      this.setmarker(latLng)
+      
+    }
+   })
   }
+setmarker(latLng:LatLngExpression){
+  if(this.currentMarker){
+    this.currentMarker.setLatLng(latLng);
+    return;
+  }
+  this.currentMarker=marker(latLng,{
+    draggable:true,
+    icon:this.MARKER_ICON
+  }).addTo(this.map)
+}
 
 }
